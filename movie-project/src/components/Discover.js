10352367d099833a2d0e245ec39b2cbcfcab0143
Jonathan Olsson <jo223ds@student.lnu.se>
2023-01-30 14:55:53 +0100

    Add: categories to choose and render movies

--- movie-project/src/components/Discover.js
+++ movie-project/src/components/Discover.js
@@ -1,9 +1,30 @@
-import React from 'react'
-import { Grid, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Grid, Typography, Button, ButtonGroup } from '@mui/material'
 import MySidebar from './Sidebar'
 import SearchField from './SearchField'
+import useFetch from './useFetch'
+import axios from 'axios'
+import '../App.css'
 
 const Discover = () => {
+  const [genres, isLoadingGenres, genresError] = useFetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`)
+  const [category, setCategory] = useState(28)
+  const [movies, setMovies] = useState([])
+  const imageUrl = 'https://image.tmdb.org/t/p/original'
+
+  useEffect(() => {
+    async function getMoviesByGenre() {
+      const response = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&with_genres=${category} `)
+      setMovies(response.data.results)
+      console.log(response.data.results)
+    }
+    getMoviesByGenre()
+  }, [category])
+
+  const handleGenreButton = (e) => {
+    setCategory(e.target.name)
+  }
+
   return (
     <Grid container>
       <Grid md={12}>
@@ -15,6 +36,24 @@ const Discover = () => {
             <Grid item md={10}>
               <SearchField />
               <Typography my={4} variant='h4'>Discover</Typography>
+              {genresError && <div>{genresError}</div>}
+              {isLoadingGenres ? <p>Loading...</p> : (
+                genres.genres.map((genre) => (
+                  <Button name={genre.id} onClick={(e) => handleGenreButton(e)} sx={{ margin: '4px' }} variant='outlined'>{genre.name}</Button>
+                )))}
+              <Grid container my={4} spacing={2}>
+                {movies.map((movie) => (
+                  <Grid item xs={6} md={2}>
+                    <div className='imgContainer'>
+                      <img src={`${imageUrl}/${movie.poster_path}`} alt={`${movie.title}`} className='movieImg' style={{ height: 'auto', width: '100%', borderRadius: '5px' }} />
+                      <div className='middle'>
+                        <Typography className='textOnImg' variant='h5'>{movie.title}</Typography>
+                        <Typography className='textOnImg' variant='h9'>{movie.release_date.split('-')[0]}</Typography>
+                      </div>
+                    </div>
+                  </Grid>
+                ))}
+              </Grid>
             </Grid>
           </Grid>
         </div>
