04daf219c220269a2f2c28bb9f2dee849e07414e
Jonathan Olsson <jo223ds@student.lnu.se>
2023-02-13 15:28:44 +0100

    Update: relative path for placeholder img

--- movie-project/src/components/Movie.js
+++ movie-project/src/components/Movie.js
@@ -10,7 +10,7 @@ const Movie = ({ movie }) => {
     <Grid item xs={6} md={2}>
       <div className='imgContainer'>
         <Link to={`/movie/${movie.id}`}>
-          <img src={movie.poster_path === null ? './movie-project/img/posterplaceholder.jpg' : `${imageUrl}/${movie.poster_path}`} alt={`${movie.title}`} className='movieImg' style={{ height: 'auto', width: '100%', borderRadius: '5px' }} />
+          <img src={movie.poster_path === null ? './img/posterplaceholder.jpg' : `${imageUrl}/${movie.poster_path}`} alt={`${movie.title}`} className='movieImg' style={{ height: 'auto', width: '100%', borderRadius: '5px' }} />
           <div className='middle'>
             <Typography className='textOnImg' variant='h5'>{movie.title}</Typography>
             <Typography className='textOnImg' variant='h9'>{movie.release_date.split('-')[0]}</Typography>
